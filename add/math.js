9a1832f2a1fe4c3e710e13ae5bba63cf9e6dfe7f
Shivani Yadav <150349374+ishivaniyadav@users.noreply.github.com>
2025-07-25 21:56:13 +0530

    importing addition function

--- add/math.js
+++ add/math.js
@@ -1,9 +1,9 @@
 const http = require('http');
 const add = require('./app'); 
 
-http.createServer((req, res) => {
+http.createServer((req, res) =>{
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end(add(201, 887).toString()); 
-}).listen(8085);
+}).listen(8085);  
 
 console.log('Server running at http://localhost:8085/');
