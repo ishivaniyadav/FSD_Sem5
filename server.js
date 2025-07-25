5797ebb8cb6ac0a676f8872ccf1d30c0fbf05983
Shivani Yadav <150349374+ishivaniyadav@users.noreply.github.com>
2025-07-25 21:57:05 +0530

    display message

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World!');
+  res.end('Hello World! ');
 }).listen(8085);
 
 console.log('Server running at http://localhost:8085/');
