875c82b5b0a91d15401892ba3dfef2ab66f5a3ab
Shivani Yadav <150349374+ishivaniyadav@users.noreply.github.com>
2025-07-28 23:58:12 +0530

    objects and arrays

---
+++ array-program.js
@@ -0,0 +1,17 @@
+Objects & Arrays
+
+// Object
+const user = {
+  name: 'Shivani',
+  age: 20,
+  greet() {
+    console.log(`Hi, I'm ${this.name}`);
+  }
+};
+
+// Array
+const colors = ['violet', 'indigo', 'magenta'];
+
+// Array methods (ES6+)
+colors.forEach(color => console.log(color));
+const lengths = colors.map(color => color.length);
