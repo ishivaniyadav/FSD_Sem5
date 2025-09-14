72bfe12a55af2a7f6147c9c6bcf877040fbe0a12
Shivani Yadav <ishivaniyadav7@gmail.com>
2025-09-14 20:34:09 +0530

    resolved all conflicts

--- task-manager/controllers/taskController.js
+++ task-manager/controllers/taskController.js
@@ -8,7 +8,6 @@ const getAllTasks = (req, res) => {
 const addTask = (req, res) => {
   const { error } = taskSchema.validate(req.body);
   if (error) return res.status(400).json({ message: error.details[0].message });
-
   const task = taskService.createTask(req.body);
   res.status(201).json(task);
 };
